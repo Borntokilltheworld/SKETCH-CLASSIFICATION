1395ddc6e1a3c35f007357da82bff69907918b87
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2021-12-20 10:48:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,38 @@ timer_check = "" ;
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+function setup(){
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("yellow")
+}
+function draw(){
+    check_sketch()
+    if(drawn_sketch == sketch){
+        answer_holder = "set";
+        score++;
+        document.getElementById("score").innerHTML = "Score:: "+score
+    }
+}
+function updateCanvas(){
+    random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+    console.log(quick_draw_data_set[random_no]);
+    sketch = quick_draw_data_set[random_no];
+  
+  document.getElementById("sketchtobedrawn").innerHTML = "sketch to be drawn:"+sketch;
+}
+function check_sketch(){
+    timer_counter++;
+    document.getElementById("timer").innerHTML = "Timer "+timer_counter;
+    console.log(timer_counter);
+    if(timer_counter>120){
+        timer_counter = 0;
+        timer_check = "completed";
+
+    }
+    if(timer_check == "completed" || answer_holder == "set"){
+        timer_check = "";
+        answer_holder = "";
+        updateCanvas();
+    }
+}
